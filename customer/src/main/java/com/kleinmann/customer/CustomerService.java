b1828d3988e41a2be6c9f7c92edc79409a5e630e
Luca Omini <lucaomini751@gmail.com>
2023-01-08 21:35:10 +0100

    customer fraud service: - added controller and service to check if  user is fraudulent - added jpa repository to connect with postgres DB

--- customer/src/main/java/com/kleinmann/customer/CustomerService.java
+++ customer/src/main/java/com/kleinmann/customer/CustomerService.java
@@ -1,9 +1,13 @@
 package com.kleinmann.customer;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Objects;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+public record CustomerService(CustomerRepository customerRepository, RestTemplate restTemplate) {
+
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -14,7 +18,20 @@ public record CustomerService(CustomerRepository customerRepository) {
         // todo: check if email is valid
         // todo check if email is not taken
 
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);
+
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+
+        if (Objects.requireNonNull(fraudCheckResponse).isFraudster()) {
+            throw new IllegalStateException("fraudster detected");
+        }
+
+
+        // todo: send notification
 
     }
 }
