6ee93a2489dccd946edcd13d87fc20f3e4138f26
Luca Omini <lucaomini751@gmail.com>
2023-01-13 19:33:54 +0100

    Added k8s configuration files for bootstrap application and services

--- customer/src/main/java/com/kleinmann/customer/CustomerApplication.java
+++ customer/src/main/java/com/kleinmann/customer/CustomerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {"com.kleinmann.customer", "com.kleinmann.amqp"}
@@ -11,6 +13,11 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableEurekaClient
 @EnableFeignClients(
         basePackages = "com.kleinmann.clients"
+)
+@PropertySources(
+        {
+                @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+        }
 )
 public class CustomerApplication {
     public static void main(String[] args) {
